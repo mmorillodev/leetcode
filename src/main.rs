a219349121fc30a3e85f874d6ce92c8134a140d8
Matheus Morillo <matheus.morillo2000@gmail.com>
2023-09-12 15:49:37 -0300

    improve solution by adding to the indexed map as soon as we interate over the vector

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,19 @@
-use std::{
-    collections::{hash_map::Entry, HashMap},
-    vec,
-};
-
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut indexed = Self::create_position_indexes(&nums);
+        use std::collections::HashMap;
 
-        for (i, num) in nums.iter().enumerate() {
-            let missing = target - num;
+        let mut map: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
 
-            if let Entry::Occupied(e) = indexed.entry(missing) {
-                if let Some(right_position) = e.get().iter().find(|curr_pos| **curr_pos != i as i32)
-                {
-                    return vec![i as i32, *right_position];
+        for (i, v) in nums.iter().enumerate() {
+            match map.get(&(target - *v)) {
+                Some(&p) => return vec![p as i32, i as i32],
+                None => {
+                    map.insert(*v, i);
                 }
             }
         }
 
-        vec![]
-    }
-
-    fn create_position_indexes(nums: &Vec<i32>) -> HashMap<i32, Vec<i32>> {
-        let mut map: HashMap<i32, Vec<i32>> = HashMap::new();
-
-        for (i, current_num) in nums.iter().enumerate() {
-            match map.entry(*current_num) {
-                Entry::Vacant(e) => {
-                    e.insert(vec![i as i32]);
-                }
-                Entry::Occupied(mut e) => {
-                    let positions = e.get_mut();
-                    positions.push(i as i32);
-                }
-            }
-        }
-
-        return map;
+        panic!("No solution found!");
     }
 }
 
@@ -47,24 +24,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_a_map_for_each_element_with_their_positions() {
-        let vector = vec![2, 7, 7, 11, 15];
-        let map = Solution::create_position_indexes(&vector);
-
-        assert_eq!(map.get(&2), Option::Some(vec![0]).as_ref());
-        assert_eq!(map.get(&7), Option::Some(vec![1, 2]).as_ref());
-        assert_eq!(map.get(&11), Option::Some(vec![3]).as_ref());
-        assert_eq!(map.get(&15), Option::Some(vec![4]).as_ref());
-    }
-
-    #[test]
     fn return_two_sum() {
         assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
         assert_eq!(Solution::two_sum(vec![5, 1, 7, 3], 10), vec![2, 3]);
         assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
         assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
         assert_eq!(Solution::two_sum(vec![7, 7, 7, 7, 7, 7], 14), vec![0, 1]);
-        assert_eq!(Solution::two_sum(vec![3, 3], 9), vec![]);
+    }
+
+    #[test]
+    #[should_panic(expected = "No solution found!")]
+    fn panic_when_no_solution_found() {
+        Solution::two_sum(vec![1, 2], 14);
     }
 }
 
